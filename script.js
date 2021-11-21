4c5ad10d54a43c95163c05a65eb6545f27438ae1
JagTheFriend <jagthefriend12@gmail.com>
2021-11-21 11:02:13 +0200

    Added zoom in and out functions

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 mapboxgl.accessToken = "";
-var map = new mapboxgl.Map({
-    container: "map",
-    style: "mapbox://styles/mapbox/streets-v11",
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+    enableHighAccuracy: true,
 });
+
+function successLocation(position) {
+    setupMap([position.coords.longitude, position.coords.latitude]);
+}
+
+function errorLocation() {
+    // default to London
+    setupMap([51.507351, -0.127758]);
+}
+
+function setupMap(center) {
+    const map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: center,
+        zoom: 17,
+    });
+    const nav = new mapboxgl.NavigationControl();
+    map.addControl(nav);
+
+    var directions = new MapboxDirections({
+        accessToken: mapboxgl.accessToken,
+    });
+
+    map.addControl(directions, "top-left");
+}
